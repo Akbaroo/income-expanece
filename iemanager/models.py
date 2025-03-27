1a8964f0b360803580abba6383b7046da144270e
akbar <akbrg0872@gmail.com>
2025-03-27 16:24:10 -0700

    model for transaction

--- iemanager/models.py
+++ iemanager/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+User = get_user_model()
+
+class Transaction(models.Model):
+    TRANSACTION_TYPES = [
+        ('income', 'Income'),  # درآمد
+        ('expense', 'Expense')  # هزینه
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE) 
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    transaction_type = models.CharField(max_length=7, choices=TRANSACTION_TYPES)  
+    date = models.DateTimeField(auto_now_add=True)  
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.user} - {self.transaction_type} - {self.amount}"
